602352ec17e79d04c6b69e8f45f4ed0324bba89b
Timothy-Lu-8 <lu.timothy.9@gmail.com>
2023-12-18 19:21:00 -0600

    Update main.py
    Updated to use and work with env files.

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -1,6 +1,9 @@
 import nextcord
 import os
 from nextcord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = nextcord.Intents.default()
 intents.message_content = True
@@ -20,5 +23,4 @@ if __name__ == '__main__':
     for extension in initial_extensions:
         client.load_extension(extension)
 
-client.run('') #removed token for security purposes
-
+client.run(os.getenv("discord_key"))
